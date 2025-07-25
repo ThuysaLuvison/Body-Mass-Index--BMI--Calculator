7d51ae5ac8a1bb92f072e36a7436f2b3448f73cd
ThuysaLuvison <thuysaluv@hotmail.com>
2025-07-25 20:59:48 -0300

    Fix: respectivas cores para cada resultado

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 function calcularIMC(){
     const peso = parseFloat(document.getElementById("peso").value);
     const altura = parseFloat(document.getElementById("altura").value);
+     const resultado = document.getElementById("resultado");
+
+       resultado.className = "";
 
     if (!peso || !altura || altura <=0 || peso <=0) {
         alert("Preencha os campos corretamente!");
@@ -13,27 +16,37 @@ function calcularIMC(){
 
     const imc = peso / (altura * altura);
     let classificacao = "";
-
-
+    let classeCor
+    
     if (imc < 18.5){
         classificacao = "Abaixo do peso!";
+        classeCor = "abaixo";
     } 
     else if (imc <24.9){
         classificacao = "Peso normal!";
+        classeCor = "normal";
     }
     else if (imc <29.9){
         classificacao = "Sobrepeso!";
+        classeCor = "sobrepeso";
     }
     else if (imc <34.9){
         classificacao = "Obesidade grau 1!";
+        classeCor = "obesidade1";
     }
     else if (imc <39.9){
         classificacao = "Obesidade grau 2!";
+        classeCor = "obesidade2";
     }
     else {
         classificacao = "Obesidade grau 3!";
+        classeCor = "obesidade3";
     }
 document.getElementById("resultado").textContent =
 `🧮 Seu IMC é ${imc.toFixed(2)} - ${classificacao}`;
+
+
+resultado.classList.add(classeCor);
+
 
 }
